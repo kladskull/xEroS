bf06e31f33f2f24d765d33c08212b390cb7b1331
Mike Curry <mikecurry74@gmail.com>
2022-03-01 16:59:34 -0800

    moving temp files

---
+++ temporary-scripts/README.md
@@ -0,0 +1,4 @@
+# This directory
+
+This directory will be removed once the programs contained are implemented into the project. They're older scripts 
+from an earlier version of Xeros.
--- temporary-scripts/submit_transaction.php
+++ temporary-scripts/submit_transaction.php
@@ -1,7 +1,9 @@
 <?php declare(strict_types=1);
 
-require __DIR__ . '/../../vendor/autoload.php';
-require __DIR__ . '/../../bootstrap.php';
+namespace Xeros;
+
+require __DIR__ . '../vendor/autoload.php';
+require __DIR__ . '../bootstrap.php';
 
 $miningUrl = 'http://localhost/';
 
